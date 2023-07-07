f6448382bba6d33ffd9ffd6932dafdd3f31d16dc
Carlos Santana <csantana23@gmail.com>
2023-07-07 18:48:52 -0400

    rename service_account vars
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/eks/terraform/examples/shell_script/main.tf
+++ argocd/eks/terraform/examples/shell_script/main.tf
@@ -12,43 +12,45 @@ locals {
     cluster_name = module.eks.cluster_name,
     region       = local.region
 
-    cert_manager_iam_role_arn = module.eks_blueprints_addons.cert_manager.iam_role_arn
-    cert_manager_namespace    = module.eks_blueprints_addons.cert_manager.namespace
+    cert_manager_iam_role_arn    = module.eks_blueprints_addons.cert_manager.iam_role_arn
+    cert_manager_namespace       = module.eks_blueprints_addons.cert_manager.namespace
+    cert_manager_service_account = module.eks_blueprints_addons.cert_manager.service_account
 
-    cluster_autoscaler_iam_role_arn = module.eks_blueprints_addons.cluster_autoscaler.iam_role_arn
-    cluster_autoscaler_sa           = module.eks_blueprints_addons.cluster_autoscaler.service_account
-    cluster_autoscaler_image_tag    = module.eks_blueprints_addons.cluster_autoscaler.image_tag
-    cluster_autoscaler_namespace    = module.eks_blueprints_addons.cluster_autoscaler.namespace
+    cluster_autoscaler_iam_role_arn    = module.eks_blueprints_addons.cluster_autoscaler.iam_role_arn
+    cluster_autoscaler_namespace       = module.eks_blueprints_addons.cluster_autoscaler.namespace
+    cluster_autoscaler_service_account = module.eks_blueprints_addons.cluster_autoscaler.service_account
+    cluster_autoscaler_image_tag       = module.eks_blueprints_addons.cluster_autoscaler.image_tag
 
-    aws_cloudwatch_metrics_iam_role_arn = module.eks_blueprints_addons.aws_cloudwatch_metrics.iam_role_arn
-    aws_cloudwatch_metrics_sa           = module.eks_blueprints_addons.aws_cloudwatch_metrics.service_account
-    aws_cloudwatch_namespace            = module.eks_blueprints_addons.aws_cloudwatch_metrics.namespace
+    aws_cloudwatch_metrics_iam_role_arn    = module.eks_blueprints_addons.aws_cloudwatch_metrics.iam_role_arn
+    aws_cloudwatch_namespace               = module.eks_blueprints_addons.aws_cloudwatch_metrics.namespace
+    aws_cloudwatch_metrics_service_account = module.eks_blueprints_addons.aws_cloudwatch_metrics.service_account
 
     aws_efs_csi_driver_iam_role_arn                = module.eks_blueprints_addons.aws_efs_csi_driver.iam_role_arn
+    aws_efs_csi_driver_namespace                   = module.eks_blueprints_addons.aws_efs_csi_driver.namespace
     aws_efs_csi_driver_controller_service_account  = module.eks_blueprints_addons.aws_efs_csi_driver.controller_service_account
     aws_efs_csi_driver_node_service_account        = module.eks_blueprints_addons.aws_efs_csi_driver.node_service_account
-    aws_efs_csi_driver_namespace                   = module.eks_blueprints_addons.aws_efs_csi_driver.namespace
 
     aws_fsx_csi_driver_iam_role_arn                = module.eks_blueprints_addons.aws_fsx_csi_driver.iam_role_arn
+    aws_fsx_csi_driver_namespace                   = module.eks_blueprints_addons.aws_fsx_csi_driver.namespace
     aws_fsx_csi_driver_controller_service_account  = module.eks_blueprints_addons.aws_fsx_csi_driver.controller_service_account
     aws_fsx_csi_driver_node_service_account        = module.eks_blueprints_addons.aws_fsx_csi_driver.node_service_account
-    aws_fsx_csi_driver_namespace                   = module.eks_blueprints_addons.aws_fsx_csi_driver.namespace
 
     aws_privateca_issuer_iam_role_arn     = module.eks_blueprints_addons.aws_privateca_issuer.iam_role_arn
-    aws_privateca_issuer_service_account  = module.eks_blueprints_addons.aws_privateca_issuer.service_account
     aws_privateca_issuer_namespace        = module.eks_blueprints_addons.aws_privateca_issuer.namespace
+    aws_privateca_issuer_service_account  = module.eks_blueprints_addons.aws_privateca_issuer.service_account
 
     external_dns_iam_role_arn     = module.eks_blueprints_addons.external_dns.iam_role_arn
-    external_dns_service_account  = module.eks_blueprints_addons.external_dns.service_account
     external_dns_namespace        = module.eks_blueprints_addons.external_dns.namespace
+    external_dns_service_account  = module.eks_blueprints_addons.external_dns.service_account
 
     external_secrets_iam_role_arn     = module.eks_blueprints_addons.external_secrets.iam_role_arn
-    external_secrets_service_account  = module.eks_blueprints_addons.external_secrets.service_account
     external_secrets_namespace        = module.eks_blueprints_addons.external_secrets.namespace
+    external_secrets_service_account  = module.eks_blueprints_addons.external_secrets.service_account
 
     aws_load_balancer_controller_iam_role_arn     = module.eks_blueprints_addons.aws_load_balancer_controller.iam_role_arn
-    aws_load_balancer_controller_service_account  = module.eks_blueprints_addons.aws_load_balancer_controller.service_account
     aws_load_balancer_controller_namespace        = module.eks_blueprints_addons.aws_load_balancer_controller.namespace
+    aws_load_balancer_controller_service_account  = module.eks_blueprints_addons.aws_load_balancer_controller.service_account
+
 
     enable_argocd = local.enable_argocd
   }
