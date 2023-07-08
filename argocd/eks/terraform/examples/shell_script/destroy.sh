d923eda5225b09f93a1eee9281b0badf9e3183a3
Carlos Santana <csantana23@gmail.com>
2023-07-08 14:44:56 -0400

    add destroy script
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

---
+++ argocd/eks/terraform/examples/shell_script/destroy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+terraform destroy -target="module.eks_blueprints_addons" -auto-approve
+terraform destroy -target="module.eks" -auto-approve
+terraform destroy -target="module.vpc" -auto-approve
+terraform destroy -auto-approve
