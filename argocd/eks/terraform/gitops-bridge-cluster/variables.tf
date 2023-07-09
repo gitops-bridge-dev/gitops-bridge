0ec8c8cc427f2b85a7343462e3f068bb93a3c968
Carlos Santana <csantana23@gmail.com>
2023-07-09 17:46:17 -0400

    create module gitops-bridge
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

---
+++ argocd/eks/terraform/gitops-bridge-cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+  default = null
+}
+
+variable "environment" {
+  default = null
+}
+
+variable "eks_blueprints_addons" {
+  default = {}
+}
+
+variable "addons" {
+  default = {}
+}
