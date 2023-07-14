85ce4b74660c5bded873b6b411217c5425774c1f
Carlos Santana <csantana23@gmail.com>
2023-07-13 21:58:23 -0400

    update destroy
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/eks/terraform/examples/cluster-1-staging/destroy.sh
+++ argocd/eks/terraform/examples/cluster-1-staging/destroy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+terraform destroy -target="shell_script.argocd_create_cluster" -auto-approve
 terraform destroy -target="module.eks_blueprints_addons" -auto-approve
 terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
