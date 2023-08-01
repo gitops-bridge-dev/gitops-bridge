6587ab3fddeff235c3a7b9fb6634085578c0bafd
Carlos Santana <csantana23@gmail.com>
2023-08-01 14:30:54 -0400

    fix destroy for multi-cluster with workspaces
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/multi-cluster-distributed/destroy.sh
+++ argocd/iac/terraform/examples/eks/multi-cluster-distributed/destroy.sh
@@ -13,8 +13,8 @@ echo "Destroying $env ..."
 set -x
 
 terraform workspace select $env
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
+terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve -var-file="${env}.tfvars"
+terraform destroy -target="module.eks_blueprints_addons" -auto-approve -var-file="${env}.tfvars"
+terraform destroy -target="module.eks" -auto-approve -var-file="${env}.tfvars"
+terraform destroy -target="module.vpc" -auto-approve -var-file="${env}.tfvars"
+terraform destroy -auto-approve -var-file="${env}.tfvars"
