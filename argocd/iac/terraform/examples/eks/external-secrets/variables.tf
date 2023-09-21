b37b65e579781b7b8b0b69db511a3707b17d26e3
Carlos Santana <csantana23@gmail.com>
2023-09-21 11:16:56 -0400

    update external secrets private-git
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/external-secrets/variables.tf
+++ argocd/iac/terraform/examples/eks/external-secrets/variables.tf
@@ -1,37 +1,46 @@
 variable "gitops_addons_org" {
   description = "Git repository org/user contains for addons"
+  type        = string
   default     = "https://github.com/gitops-bridge-dev"
 }
 variable "gitops_addons_repo" {
   description = "Git repository contains for addons"
+  type        = string
   default     = "gitops-bridge-argocd-control-plane-template"
 }
 variable "gitops_addons_basepath" {
   description = "Git repository base path for addons"
+  type        = string
   default     = ""
 }
 variable "gitops_addons_path" {
   description = "Git repository path for addons"
+  type        = string
   default     = "bootstrap/control-plane/addons"
 }
 variable "gitops_addons_revision" {
   description = "Git repository revision/branch/ref for addons"
+  type        = string
   default     = "HEAD"
 }
 
 variable "gitops_workload_org" {
   description = "Git repository org/user contains for workload"
+  type        = string
   default     = "https://github.com/gitops-bridge-dev"
 }
 variable "gitops_workload_repo" {
   description = "Git repository contains for workload"
+  type        = string
   default     = "gitops-bridge"
 }
 variable "gitops_workload_path" {
   description = "Git repository path for workload"
+  type        = string
   default     = "argocd/iac/terraform/examples/eks/external-secrets/k8s"
 }
 variable "gitops_workload_revision" {
   description = "Git repository revision/branch/ref for workload"
+  type        = string
   default     = "HEAD"
 }
