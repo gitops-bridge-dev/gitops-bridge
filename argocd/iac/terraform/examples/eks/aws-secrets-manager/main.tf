cecb9ed6c80038aa24bb19775bfb8813a14a2c78
Carlos Santana <csantana23@gmail.com>
2023-10-28 22:48:45 -0400

    update crossplane
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/aws-secrets-manager/main.tf
+++ argocd/iac/terraform/examples/eks/aws-secrets-manager/main.tf
@@ -41,8 +41,6 @@ locals {
   vpc_cidr = var.vpc_cidr
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-
-
   gitops_addons_url      = "${var.gitops_addons_org}/${var.gitops_addons_repo}"
   gitops_addons_basepath = var.gitops_addons_basepath
   gitops_addons_path     = var.gitops_addons_path
--- argocd/iac/terraform/examples/eks/getting-started/main.tf
+++ argocd/iac/terraform/examples/eks/getting-started/main.tf
@@ -123,7 +123,7 @@ locals {
 
   tags = {
     Blueprint  = local.name
-    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+    GithubRepo = "github.com/gitops-bridge-dev/gitops-bridge"
   }
 }
 
