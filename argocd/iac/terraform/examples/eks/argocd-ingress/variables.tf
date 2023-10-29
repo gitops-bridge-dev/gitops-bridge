286ed788072f6241380079d1ae59268f7f2e54eb
Carlos Santana <csantana23@gmail.com>
2023-10-28 22:21:53 -0400

    updated argocd-ingress
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/argocd-ingress/variables.tf
+++ argocd/iac/terraform/examples/eks/argocd-ingress/variables.tf
@@ -2,6 +2,30 @@ variable "domain_name" {
   description = "Route 53 domain name"
   type        = string
 }
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = any
+  default = {
+    enable_external_dns = true
+    enable_aws_load_balancer_controller               = true
+    enable_aws_argocd_ingress = true
+  }
+}
 variable "gitops_addons_org" {
   description = "Git repository org/user contains for addons"
   default     = "https://github.com/gitops-bridge-dev"
