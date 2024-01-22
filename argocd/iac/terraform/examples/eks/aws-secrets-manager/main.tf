656c786e7c9d5f4b3541953dcc4a04580098ee0e
Carlos Santana <csantana23@gmail.com>
2024-01-22 17:59:07 -0500

    update gitops-bridge refernce from git to terraform registry (#46)
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    Signed-off-by: Blake Romano <blake.romano@imaginelearning.com>

--- argocd/iac/terraform/examples/eks/aws-secrets-manager/main.tf
+++ argocd/iac/terraform/examples/eks/aws-secrets-manager/main.tf
@@ -128,7 +128,7 @@ locals {
 # GitOps Bridge: Bootstrap
 ################################################################################
 module "gitops_bridge_bootstrap" {
-  source = "github.com/gitops-bridge-dev/gitops-bridge-argocd-bootstrap-terraform?ref=v2.0.0"
+  source = "gitops-bridge-dev/gitops-bridge/helm"
 
   cluster = {
     cluster_name = module.eks.cluster_name
