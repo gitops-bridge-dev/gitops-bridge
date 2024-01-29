6e96ab8888a7088bde5fef9dd0c4d1878a670906
Carlos Santana <csantana23@gmail.com>
2024-01-29 01:21:21 -0500

    Karpenter pattern (#50)
    * update argo workflows
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * add karpenter pattern
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * make eks 1.29
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    ---------
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

---
+++ argocd/iac/terraform/examples/eks/karpenter/variables.tf
@@ -0,0 +1,77 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.29"
+}
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = any
+  default = {
+    enable_aws_load_balancer_controller = true
+    enable_aws_ebs_csi_resources        = true # generate gp2 and gp3 storage classes for ebs-csi
+    enable_karpenter                    = true
+  }
+}
+# Addons Git
+variable "gitops_addons_org" {
+  description = "Git repository org/user contains for addons"
+  type        = string
+  default     = "https://github.com/gitops-bridge-dev"
+}
+variable "gitops_addons_repo" {
+  description = "Git repository contains for addons"
+  type        = string
+  default     = "gitops-bridge-argocd-control-plane-template"
+}
+variable "gitops_addons_revision" {
+  description = "Git repository revision/branch/ref for addons"
+  type        = string
+  default     = "main"
+}
+variable "gitops_addons_basepath" {
+  description = "Git repository base path for addons"
+  type        = string
+  default     = ""
+}
+variable "gitops_addons_path" {
+  description = "Git repository path for addons"
+  type        = string
+  default     = "bootstrap/control-plane/addons"
+}
+
+# Workloads Git
+variable "gitops_workload_org" {
+  description = "Git repository org/user contains for workload"
+  type        = string
+  default     = "https://github.com/gitops-bridge-dev"
+}
+variable "gitops_workload_repo" {
+  description = "Git repository contains for workload"
+  type        = string
+  default     = "gitops-bridge"
+}
+variable "gitops_workload_revision" {
+  description = "Git repository revision/branch/ref for workload"
+  type        = string
+  default     = "main"
+}
+variable "gitops_workload_basepath" {
+  description = "Git repository base path for workload"
+  type        = string
+  default     = "argocd/iac/terraform/examples/eks/"
+}
+variable "gitops_workload_path" {
+  description = "Git repository path for workload"
+  type        = string
+  default     = "karpenter/k8s"
+}
