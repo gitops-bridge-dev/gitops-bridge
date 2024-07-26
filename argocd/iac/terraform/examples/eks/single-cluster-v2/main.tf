e229d3fd79d9252df14fed6442447298b3539c64
Carlos Santana <csantana23@gmail.com>
2024-07-25 22:14:39 -0400

    update netpol
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/single-cluster-v2/main.tf
+++ argocd/iac/terraform/examples/eks/single-cluster-v2/main.tf
@@ -261,7 +261,8 @@ module "eks" {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
-        }
+        },
+        enableNetworkPolicy : "true"
       })
     }
     aws-ebs-csi-driver = {
