f686cd74e77915b44e9be4332b26638349253767
Carlos Santana <csantana23@gmail.com>
2024-08-08 13:56:24 -0400

    update destroy
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- argocd/iac/terraform/examples/eks/single-cluster-v2/destroy.sh
+++ argocd/iac/terraform/examples/eks/single-cluster-v2/destroy.sh
@@ -28,11 +28,13 @@ scale_down_karpenter_nodes() {
       replicaset_name=$(kubectl get pod $pod_name -n $namespace -o jsonpath='{.metadata.ownerReferences[?(@.kind=="ReplicaSet")].name}')
       deployment_name=$(kubectl get replicaset $replicaset_name -n $namespace -o jsonpath='{.metadata.ownerReferences[?(@.kind=="Deployment")].name}')
       if [[ $(kubectl get deployment $deployment_name -n $namespace -o jsonpath='{.spec.replicas}') -gt 0 ]]; then
+        echo kubectl scale deployment $deployment_name -n $namespace --replicas=0
         kubectl scale deployment $deployment_name -n $namespace --replicas=0
       fi
       elif echo $owner_refs | grep -q "StatefulSet"; then
       statefulset_name=$(kubectl get pod $pod_name -n $namespace -o jsonpath='{.metadata.ownerReferences[?(@.kind=="StatefulSet")].name}')
       if [[ $(kubectl get statefulset $statefulset_name -n $namespace -o jsonpath='{.spec.replicas}') -gt 0 ]]; then
+        echo kubectl scale statefulset $statefulset_name -n $namespace --replicas=0
         kubectl scale statefulset $statefulset_name -n $namespace --replicas=0
       fi
       fi
@@ -51,13 +53,10 @@ scale_down_karpenter_nodes() {
     sleep 60
     nodes=$(kubectl get nodes -l karpenter.sh/registered=true -o jsonpath='{.items[*].metadata.name}')
   done
+  sleep 60
 
 
 }
-
-# We must destroy the karpenter node before we destroy the EKS cluster
-
-exit 0
 
 # Delete the Ingress/SVC before removing the addons
 TMPFILE=$(mktemp)
@@ -68,7 +67,7 @@ if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
   scale_down_karpenter_nodes
   kubectl delete ing -A --all
   # delete all the kuberneters service of type LoadBalancer, without using jq
-  kubectl get svc --all-namespaces -o json | grep -E '"type": "LoadBalancer"' | awk '{print "echo kubectl delete svc " $1 " -n " $2}' | bash
+  kubectl get svc --all-namespaces -o json | grep -E '"type": "LoadBalancer"' | awk '{print "kubectl delete svc " $1 " -n " $2}' | bash
   sleep 60
 fi
 
@@ -77,167 +76,3 @@ terraform destroy -target="module.eks_blueprints_addons" -auto-approve
 terraform destroy -target="module.eks" -auto-approve
 terraform destroy -target="module.vpc" -auto-approve
 terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-# Delete the Ingress/SVC before removing the addons
-TMPFILE=$(mktemp)
-terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
-# check if TMPFILE contains the string "No outputs found"
-if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
-  source "$TMPFILE"
-  kubectl scale deploy -n game-2048 game-2048 --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
-  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
-  kubectl scale deploy -n kube-system metrics-server --replicas=0
-  sleep 60
-  kubectl delete nodes -l karpenter.sh/registered=true
-  kubectl delete -n game-2048 ing game-2048
-  kubectl delete -n argocd svc argocd-server
-  sleep 60
-fi
-
-terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
-terraform destroy -target="module.eks_blueprints_addons" -auto-approve
-terraform destroy -target="module.eks" -auto-approve
-terraform destroy -target="module.vpc" -auto-approve
-terraform destroy -auto-approve
-
-
-
